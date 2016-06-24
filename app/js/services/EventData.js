edb340db73ccedeed75b777245ed01e6e6ccb035
Daniel Chia <daniel_1118@hotmail.com>
2016-06-23 23:35:52 -0400

    added post functionality

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,12 +1,18 @@
 'use strict';
 
 eventsApp.factory('eventData', function($resource){
+
+	var resource = $resource('/data/event/:id', {id: '@id'});
+	
 	return {
 		
 		//using promises..
 		getEvent: function(){
-			//return $http({method: 'GET', url: '/data/event/1'});
-			return $resource('/data/event/:id', {id: '@id'}).get({id: 1});
+			return resource.get({id: 1});
+		},
+		save: function(event){
+			event.id = 999;
+			return resource.save(event);	
 		}
 
 	};

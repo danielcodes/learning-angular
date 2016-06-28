ccfa8fa54f4560c4a126f3ceeb74a357cdbf3c27
Daniel Chia <daniel_1118@hotmail.com>
2016-06-28 17:10:18 -0400

    moved newevent.html to templates, created first route

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource']);
+var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource', 'ngRoute'])
+		.config(function($routeProvider){
+			//returns a module
+			$routeProvider.when('/newEvent',
+				{
+					templateUrl: 'templates/NewEvent.html',
+					controller: 'EditEventController'
+				});	
+		});

d871bdc5a064e9c6840b828b2d89d6fe0009f14c
Daniel Chia <daniel_1118@hotmail.com>
2016-07-03 22:39:29 -0400

    tested for save and getAllEvents methods from eventData service

--- test/unit/eventDataSpec.js
+++ test/unit/eventDataSpec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//testing all 3 methods from the eventData service
 describe('eventData', function(){
 
 	beforeEach(module('eventsApp'));
@@ -16,4 +17,40 @@ describe('eventData', function(){
 			$httpBackend.verifyNoOutstandingRequest();
 		})
 	);
+
+	it('should return correct data when getEvent is called', 
+		inject(function(eventData, $httpBackend){
+
+			$httpBackend.when('GET', '/data/event/11').respond({name: 'My Event'});
+			var event = eventData.getEvent(11);
+			$httpBackend.flush();
+			
+			expect(event.name).toBe('My Event');
+		})
+	);
+
+	it('should set id to 999 when save is called',
+		inject(function(eventData, $httpBackend){
+
+			$httpBackend.when('POST', '/data/event/999').respond({});
+			var event = {name: 'My Event'}; 
+			eventData.save(event);
+			$httpBackend.flush();
+			
+			expect(event.id).toBe(999);
+		})
+	);	
+
+	it('should issue GET to /data/event when getAllEvents is called',
+		inject(function(eventData, $httpBackend){
+
+			$httpBackend.when('GET', '/data/event').respond([{name: 'My Event'}]);
+			var events = eventData.getAllEvents(); 
+			$httpBackend.flush();
+			
+			expect(events[0].name).toBe('My Event');
+		})
+	);	
+
+
 });
